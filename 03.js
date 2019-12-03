3faff33ba875dc433726788b604ded70cdf9e244
cubehouse <me@jamie.holdings>
2019-12-03 22:35:35 +0000

    [+] Day 3 part 2

--- 03.js
+++ 03.js
@@ -11,6 +11,8 @@ class Line {
         this.maxY = Math.max(this.start.y, this.end.y);
 
         this.orientation = (this.start.x === this.end.x) ? 'V' : 'H';
+
+        this.distance = this.orientation === 'V' ? Math.abs(this.maxY - this.minY) : Math.abs(this.maxX - this.minX);
     }
 
     /** Does this line intersect with another line? Returns location of intersection, or undefined if they don't intersect */
@@ -21,6 +23,7 @@ class Line {
             return {
                 x: other.minX,
                 y: this.minY,
+                distance: Math.abs(other.minX - this.start.x),
             };
         }
 
@@ -28,6 +31,7 @@ class Line {
             return {
                 x: this.minX,
                 y: other.minY,
+                distance: Math.abs(other.minY - this.start.y),
             };
         }
 
@@ -63,6 +67,9 @@ function AddInstructionToCoord(x, y, instr) {
 }
 
 Advent.GetInput().then((input) => {
+    //input = ['R75,D30,R83,U83,L12,D49,R71,U7,L72', 'U62,R66,U55,R34,D71,R55,D58,R83'];
+    //input = ['R8,U5,L5,D3', 'U7,R6,D4,L4'];
+
     // build all our lines from input
     const lines = input.map((lines) => {
         let x = 0, y = 0;
@@ -81,26 +88,37 @@ Advent.GetInput().then((input) => {
 
     // find intersections between 1st and 2nd line sections
     const Intersections = [];
+    let L1Distance = 0;
     lines[0].forEach((l1) => {
+        let L2Distance = 0;
         lines[1].forEach((l2) => {
             const cross = l1.Intersect(l2);
-            if (cross !== undefined) {
-                if (cross.x === 0 && cross.y === 0) return;
+            if (cross !== undefined && (cross.x !== 0 || cross.y !== 0)) {
+                // need to find how far the wire steps with the opposite intersection for part 2
+                const counterCross = l2.Intersect(l1);
+                
+                cross.totalDistance = cross.distance + counterCross.distance + L1Distance + L2Distance;
+
                 Intersections.push(cross);
             }
+
+            L2Distance += l2.distance;
         });
+
+        L1Distance += l1.distance;
     });
 
     // find nearset intersection to origin
     const ShortestDist = Intersections.map(i => Math.abs(i.x) + Math.abs(i.y)).reduce((p,n) => {
         return Math.min(p, n);
     }, Number.MAX_SAFE_INTEGER);
+    
+    const ShortestDist2 = Intersections.reduce((p, n) => {
+        return n.totalDistance < p.totalDistance ? n : p;
+    }, Intersections[0]);
 
     return Advent.Submit(ShortestDist).then(() => {
-        // TODO - puzzle answer 2
-        let answer2 = 0;
-
-        //return Advent.Submit(answer2, 2);
+        return Advent.Submit(ShortestDist2.totalDistance, 2);
     });
 }).catch((e) => {
     console.log(e);
