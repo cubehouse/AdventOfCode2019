1828f7adae8922fbfcf4aee681085ab9cf588cfd
Jamie Holding <me@jamie.holdings>
2019-12-14 11:54:27 +0000

    [~] Tidy up day 13 part 1 for part 2 work

--- 13.js
+++ 13.js
@@ -3,8 +3,10 @@ const Advent = new (require('./index.js'))(13, 2019);
 const Intcode = require('./intcode');
 
 
-class Screen {
-    constructor() {
+class Arcade {
+    constructor(PC) {
+        this.PC = PC;
+
         term.fullscreen(true);
         term.clear();
         this.maxY = 0;
@@ -46,40 +48,47 @@ class Screen {
         this.maxY = Math.max(this.maxY, y);
         term.moveTo(0, this.maxY + 3);
     }
-}
 
-Advent.GetInput().then((input) => {
-    const S = new Screen();
-    const PC = new Intcode(input);
+    BlocksLeft() {
+        return Object.keys(this.Grid).reduce((p, cell) => {
+            return p + ((this.Grid[cell] === 2) ? 1 : 0);
+        }, 0);
+    }
 
-    const DrawTile = () => {
-        return PC.GetOutput().then((x) => {
-            return PC.GetOutput().then((y) => {
-                return PC.GetOutput().then((tile) => {
-                    S.Draw(x, y, tile);
+    DrawTile() {
+        return this.PC.GetOutput().then((x) => {
+            return this.PC.GetOutput().then((y) => {
+                return this.PC.GetOutput().then((tile) => {
+                    this.Draw(x, y, tile);
                     return Promise.resolve();
                 });
             });
         });
-    };
+    }
 
-    const StepChunks = 100;
-    const Step = (rec = 0) => {
-        DrawTile().then(() => {
-            if (rec > StepChunks) {
-                process.nextTick(Step);
-            } else {
-                Step(rec++);
-            }
-        });
-    };
-    Step();
+    Run() {
+        const StepChunks = 100;
+        const Step = (rec = 0) => {
+            this.DrawTile().then(() => {
+                if (rec > StepChunks) {
+                    process.nextTick(Step);
+                } else {
+                    Step(rec++);
+                }
+            });
+        };
+        Step();
 
-    return PC.Run().then(() => {
+        return this.PC.Run();
+    }
+}
+
+Advent.GetInput().then((input) => {
+    const Part1 = new Arcade(new Intcode(input));
+
+    return Part1.Run().then(() => {
         // count blocks
-        const blocks = Object.keys(S.Grid).reduce((p, cell) => {
-            return p + ((S.Grid[cell] === 2) ? 1 : 0);
-        }, 0);
+        const blocks = Part1.BlocksLeft();
 
         return Advent.Submit(blocks).then(() => {
             console.log('Done!');
