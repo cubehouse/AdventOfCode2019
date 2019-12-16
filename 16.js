9023fd7851a1c07d7bd321e79c4182620c385a99
cubehouse <me@jamie.holdings>
2019-12-16 21:50:07 +0000

    [+] Day 16 part 2 * Tried to optimise original solution, but ultimately just threw guessing at the pattern

--- 16.js
+++ 16.js
@@ -2,34 +2,36 @@ const Advent = new (require('./index.js'))(16, 2019);
 
 class FFT {
     constructor(input, pattern = '0, 1, 0, -1') {
-        this.signal = input.split('').map(Number);
+        this.signal = Array.isArray(input) ? input : input.split('').map(Number);
         this.pattern = pattern.split(',').map(Number);
+        this.signalNum = this.signal.length;
+    }
 
-        // prebuild all our patterns for processing
-        this.patterns = this.signal.map((x, idx) => {
-            const p = [];
-            let pIdx = 0;
-            while(p.length <= this.signal.length) {
-                const patternToAdd = new Array(idx + 1).fill(this.pattern[pIdx++ % this.pattern.length]);
-                Array.prototype.push.apply(p, patternToAdd);
+    CalcIdx(idx) {
+        let signalIdx = idx;
+        let result = 0;
+        let add = true;
+        while(signalIdx < this.signalNum) {
+            const arr = this.signal.slice(signalIdx, signalIdx + idx + 1);
+            const newNum = arr.reduce((p, x) => p+x, 0);
+            if (add) {
+                result += newNum;
+                add = false;
+            } else {
+                result -= newNum;
+                add = true;
             }
-            return p.slice(1, this.signal.length + 1);
-        });
+            signalIdx += (idx + 1) * 2;
+        }
+        result = Math.abs(result % 10);
+        this.signal[idx] = result;
+        return result;
     }
 
-    PhaseIdx(idx) {
-        const pattern = this.patterns[idx];
-        const patternNum = pattern.length;
-        const Result = Math.abs(this.signal.reduce((p, x, idx) => {
-            return p + (x * pattern[idx % patternNum]);
-        }, 0) % 10);
-        return Result;
-    }
-
-    Phase() {
-        this.signal = this.signal.map((x, idx) => {
-            return this.PhaseIdx(idx);
-        });
+    Phase(startIdx = 0) {
+        for(let i=startIdx; i<this.signalNum; i++) {
+            this.CalcIdx(i);
+        }
     }
 
     PhaseX(num) {
@@ -37,34 +39,51 @@ class FFT {
             this.Phase();
         }
     }
+
+    Phase2() {
+        for(let x=0; x<100; x++) {
+            for(let i=this.signal.length - 2; i>=0; i--) {
+                this.signal[i] = (this.signal[i] + this.signal[i + 1]) % 10;
+            }
+        }
+    }
 };
 
-// Part 1 unit tests
-const Example1 = new FFT('12345678');
-Example1.Phase();
-if (Example1.signal.join('') !== '48226158') throw new Error('Example 1 failed');
-Example1.Phase();
-if (Example1.signal.join('') !== '34040438') throw new Error('Example 1 failed');
-Example1.Phase();
-if (Example1.signal.join('') !== '03415518') throw new Error('Example 1 failed');
-Example1.Phase();
-if (Example1.signal.join('') !== '01029498') throw new Error('Example 1 failed');
-const Example2 = new FFT('80871224585914546619083218645595');
-Example2.PhaseX(100);
-if (Example2.signal.slice(0, 8).join('') !== '24176176') throw new Error('Example 2 failed');
-const Example3 = new FFT('19617804207202209144916044189917');
-Example3.PhaseX(100);
-if (Example3.signal.slice(0, 8).join('') !== '73745418') throw new Error('Example 3 failed');
-const Example4 = new FFT('69317163492948606335995924319873');
-Example4.PhaseX(100);
-if (Example4.signal.slice(0, 8).join('') !== '52432133') throw new Error('Example 4 failed');
+try {
+    // Part 1 unit tests
+    const Example1 = new FFT('12345678');
+    Example1.Phase();
+    if (Example1.signal.join('') !== '48226158') throw new Error('Example 1 failed ' + Example1.signal.join(''));
+    Example1.Phase();
+    if (Example1.signal.join('') !== '34040438') throw new Error('Example 1 failed');
+    Example1.Phase();
+    if (Example1.signal.join('') !== '03415518') throw new Error('Example 1 failed');
+    Example1.Phase();
+    if (Example1.signal.join('') !== '01029498') throw new Error('Example 1 failed');
+    const Example2 = new FFT('80871224585914546619083218645595');
+    Example2.PhaseX(100);
+    if (Example2.signal.slice(0, 8).join('') !== '24176176') throw new Error('Example 2 failed');
+    const Example3 = new FFT('19617804207202209144916044189917');
+    Example3.PhaseX(100);
+    if (Example3.signal.slice(0, 8).join('') !== '73745418') throw new Error('Example 3 failed');
+    const Example4 = new FFT('69317163492948606335995924319873');
+    Example4.PhaseX(100);
+    if (Example4.signal.slice(0, 8).join('') !== '52432133') throw new Error('Example 4 failed');
+} catch(e) {
+    console.error(e);
+}
 
 // part 1
 Advent.GetInput().then((input) => {
     const Part1 = new FFT(input);
     Part1.PhaseX(100);
     return Advent.Submit(Part1.signal.slice(0, 8).join('')).then(() => {
-        console.log('Done');
+        // build new input
+        const offset = Number(input.slice(0, 7));
+        const input2 = input.repeat(10000).slice(offset).split('').map(Number);
+        const Part2 = new FFT(input2);
+        Part2.Phase2();
+        return Advent.Submit(Part2.signal.slice(0, 8).join(''), 2);
     });
 }).catch((e) => {
     console.log(e);
