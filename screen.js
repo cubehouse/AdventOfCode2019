af5bfaad9a8a3546e75dbfc84b97142500927257
cubehouse <me@jamie.holdings>
2019-12-20 21:57:42 +0000

    [!] Improve screen renderer

--- screen.js
+++ screen.js
@@ -2,7 +2,7 @@
 const { performance } = require('perf_hooks');
 const EventEmitter = require('events');
 const blessed = require('blessed');
-const program = blessed.program();
+const path = require('path');
 const util = require('util');
 
 class Screen extends EventEmitter {
@@ -15,51 +15,75 @@ class Screen extends EventEmitter {
 
         this.minX = null;
         this.minY = null;
+        this.maxX = null;
+        this.maxY = null;
 
         this.log = [];
         this.logWidth = 80;
-        console.log = (...args) => {
-            this.log.unshift(...args.map(util.inspect).map((x) => {
-                return x.slice(0, this.logWidth);
-            }));
-            this.log.splice(this.height, this.log.length - this.height);
+        this.logRedraw = false;
 
-            // redraw console
-            this.DrawLog();
-        };
+        this.screen = blessed.screen({
+            smartCSR: true,
+            log: path.join(__dirname, 'screen.log'),
+        });
+        this.logBox = blessed.text({
+            top: 0,
+            right: 0,
+            width: this.logWidth || 80,
+            height: '100%',
+            border: {
+                type: 'line'
+            },
+            style: {
+                fg: 'white',
+                bg: 'black',
+                border: {
+                    fg: '#ffa500'
+                },
+            },
+        });
+        this.screen.append(this.logBox);
         
-        this.fps = fps || 30;
+        console.log = (...args) => {
+            if (args.length > 0) {
+                args.map(util.inspect).forEach((x) => {
+                    this.logBox.unshiftLine(x);
+                });
+                this.logRedraw = true;
+            }
+        };
+
+        this.mapBox = blessed.box({
+            top: 0,
+            right: this.logWidth,
+            width: "100%",
+            scrollable: true,
+        });
+        this.screen.append(this.mapBox);
+        this.mapLines = {};
+        
+        this.fps = fps || 60;
         this.lastFrameTime = performance.now();
         this.ticks = [];
 
         process.stdout.on('resize', this.OnResize.bind(this));
         this.OnResize();
-        
-        program.alternateBuffer();
-        program.enableMouse();
-        program.hideCursor();
 
-        program.key(['q', 'C-c'], (ch, key) => {
-            this.Stop();
+        this.screen.key(['escape', 'q', 'C-c'], () => {
             process.exit(0);
         });
-        program.on('mouse', (data) => {});
         
         // start ticking
         setTimeout(this.Tick.bind(this), 0);
-    }
 
-    Stop() {
-        program.clear();
-        program.disableMouse();
-        program.showCursor();
-        program.normalBuffer();
+        this.screen.render();
     }
 
     Redraw() {
-        program.clear();
         this.draws.splice(0, this.draws.length);
-        this.draws.push(...Object.values(this.Grid));
+        for(let y=this.minY; y<=this.maxY; y++) {
+            this.draws.push(y);
+        }
     }
 
     OnResize() {
@@ -98,50 +122,45 @@ class Screen extends EventEmitter {
         this.Grid[gridKey][key] = val;
 
         if (key === 'val') {
-            if (this.minX === null || this.minX > x || this.minY > y) {
+            if (this.mapLines[y] === undefined) {
+                this.mapLines[y] = '';
+            }
+            if (this.mapLines[y].length < x) {
+                this.mapLines[y] = this.mapLines[y] + ' '.repeat(x - this.mapLines[y].length);
+            }
+            this.mapLines[y] = this.mapLines[y].substr(0, x) + val + this.mapLines[y].substr(x + 1);
+
+            if (this.minX === null || this.minX > x || this.minY > y || this.maxX < x || this.maxY < y) {
                 this.minX = Math.min(x, this.minX);
                 this.minY = Math.min(y, this.minY);
+                this.maxX = Math.max(x, this.maxX);
+                this.maxY = Math.max(y, this.maxY);
 
                 this.Redraw();
             } else {
-                this.draws.push(this.Grid[gridKey]);
+                this.draws.push(y);
             }
         }
     }
 
     Draw() {
-        if (this.draws.length === 0) return;
+        if (this.draws.length === 0 && !this.logRedraw) return;
 
-        // sort by Y and then X so our cursor move around in order
-        this.draws.sort((a ,b) => {
-            if (a.y === b.y) {
-                return a.x - b.x;
+        const boxWidth = this.screen.width - this.logWidth;
+        this.draws.forEach((y) => {
+            const frameY = y - this.minY;
+            if (this.mapLines[y]) {
+                this.mapBox.setBaseLine(frameY, this.mapLines[y].substr(0, boxWidth));
+            } else {
+                this.mapBox.setBaseLine(frameY, '');
             }
-            return a.y - b.y;
-        });
-        
-        // TODO - group draws in same line into a single draw call
-
-        // process all our draw calls
-        this.draws.forEach((draw) => {
-            const x = draw.x - this.minX;
-            const y = draw.y - this.minY;
-            if (x >= this.frameWidth || y >= this.height) {
-                return;
-            }
-            program.move(x, y);
-            program.write(draw.val);
         });
 
         // clear our draw list
         this.draws.splice(0, this.draws.length);
-    }
 
-    DrawLog() {
-        this.log.forEach((log, idx) => {
-            program.move(this.frameWidth + 1, idx);
-            program.write(log);
-        });
+        this.screen.render();
+        this.logRedraw = false;
     }
 
     Tick() {
