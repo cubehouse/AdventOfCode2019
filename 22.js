4fea70f6af380118a40f8e650c76f37ecc245378
cubehouse <me@jamie.holdings>
2019-12-24 21:03:46 +0000

    [+] Brain tickler * Started actually making the array and had huge issues * This is much easier

--- 22.js
+++ 22.js
@@ -1,15 +1,39 @@
 const Advent = new (require('./index.js'))(22, 2019);
 
+class Deck {
+    constructor(size, target, instr) {
+        this.size = size;
+        this.target = target;
+
+        instr.forEach((s) => {
+            if (s.indexOf('deal into new stack') === 0) {
+                this.NewStack();
+            } else if (s.indexOf('cut') === 0) {
+                this.Cut(Number(s.split(' ')[1]));
+            } else if (s.indexOf('deal with increment' === 0)) {
+                this.Deal(Number(s.split(' ')[3]));
+            } else {
+                console.error(`Unknown op ${s}`);
+            }
+        });
+    }
+
+    NewStack() {
+        this.target = this.size - this.target - 1;
+    }
+
+    Cut(num) {
+        this.target = (this.target - num + this.size) % this.size;
+    }
+
+    Deal(num) {
+        this.target = (this.target * num) % this.size;
+    }
+}
+
 Advent.GetInput().then((input) => {
-    // TODO - puzzle answer 1
-    let answer1 = 0;
-
-    /*return Advent.Submit(answer1).then(() => {
-        // TODO - puzzle answer 2
-        let answer2 = 0;
-
-        return Advent.Submit(answer2, 2);
-    });*/
+    const D = new Deck(10007, 2019, input);
+    return Advent.Submit(D.target);
 }).catch((e) => {
     console.log(e);
 });
