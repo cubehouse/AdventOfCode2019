438813351b41d40fa9de772e3a5a77ce458155cb
cubehouse <me@jamie.holdings>
2019-12-24 01:34:14 +0000

    [+] Day 21 part 2 * Hated this one, hurts my head

--- 21.js
+++ 21.js
@@ -17,7 +17,28 @@ AND D J
 OR T J    
 WALK`.split(/\n/g).map(PC.InputStr.bind(PC));
     return PC.Run().then(() => {
-        return Advent.Submit(PC.output);
+        return Advent.Submit(PC.output).then(() => {
+            const PC2 = new Intcode(input);
+            PC2.on('output', (out) => {
+                if (out < 128) {
+                    process.stdout.write(String.fromCharCode(out));
+                } else {
+                    console.log(out);
+                }
+            });
+            `OR A J
+            AND B J
+            AND C J
+            NOT J J
+            OR E T
+            OR H T
+            AND D T
+            AND T J
+            RUN`.split(/\n/g).map(PC2.InputStr.bind(PC2));
+            return PC2.Run().then(() => {
+                return Advent.Submit(PC2.output, 2);
+            });
+        });
     });
 }).catch((e) => {
     console.log(e);
