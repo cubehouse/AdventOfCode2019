4e0f23cd9678d58996838c39e6d2ab12d0bb96d7
cubehouse <me@jamie.holdings>
2019-12-25 21:19:46 +0000

    [+] Day 23 part 2 * Uses a quick hack of a 1 second interval for checking idle, which I would like to fix properly but want to get through day 24 and 25 tonight

--- 23.js
+++ 23.js
@@ -28,7 +28,7 @@ class NetPC extends Intcode {
             return this.GetOutput().then((X) => {
                 return this.GetOutput().then((Y) => {
                     // emit packet for main program to deal with
-                    this.emit('packet', target, X, Y);
+                    this.emit('packet', target, X, Y, this.ID);
 
                     // wait for another packet
                     setImmediate(this.WaitForPacket.bind(this));
@@ -40,28 +40,60 @@ class NetPC extends Intcode {
 
 Advent.GetInput().then((input) => {
     const AnswerPart1 = (ans) => {
-        return Advent.Submit(ans).then(() => {
-            process.exit(0);
-        });
+        return Advent.Submit(ans);
     };
+
+    let Part1Done = false;
+    let Part2Done = false;
+
+    const NATLastPacket = {
+        x: -1,
+        y: -1,
+    };
+
+    let LastSentY = -1;
 
     const PCs = new Array(50).fill(0).map((x, idx) => {
         const PC = new NetPC(input, idx);
-        PC.on('packet', (target, X, Y) => {
+        PC.on('packet', (target, X, Y, source) => {
             if (PCs[target] !== undefined) {
                 PCs[target].SendPacket(X, Y);
             } else {
-                if (target === 255) {
-                    return AnswerPart1(Y);
+                if (!Part1Done && target === 255) {
+                    Part1Done = true;
+                    AnswerPart1(Y);
                 }
-                console.log(target, X, Y);
+                if (target === 255) {
+                    NATLastPacket.x = X;
+                    NATLastPacket.y = Y;
+                } else {
+                    console.log(target, X, Y, source);
+                }
             }
         });
         PC.Run();
         return PC;
     });
 
-    
+    setInterval(() => {
+        if (!Part1Done || NATLastPacket.x < 0) return;
+        const findActivePC = PCs.find((P) => {
+            return P.inputs.filter(x => x < 0).length > 0;
+        });
+        if (findActivePC === undefined) {
+            if (NATLastPacket.y === LastSentY) {
+                if (!Part2Done) {
+                    Part2Done = true;
+                    return Advent.Submit(LastSentY, 2).then(() => {
+                        process.exit(0);
+                    });
+                }
+            }
+            LastSentY = NATLastPacket.y;
+            PCs[0].Input(NATLastPacket.x);
+            PCs[0].Input(NATLastPacket.y);
+        }
+    }, 1000);
 }).catch((e) => {
     console.log(e);
 });
