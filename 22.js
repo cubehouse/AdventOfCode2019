9dd67c80b503542646dd09330daf49f261f70a8d
cubehouse <me@jamie.holdings>
2019-12-25 19:29:20 +0000

    [~] Day 22 chaos * Didn't get on with part 2, so just tidied up part 1 loads

--- 22.js
+++ 22.js
@@ -1,11 +1,10 @@
 const Advent = new (require('./index.js'))(22, 2019);
 
 class Deck {
-    constructor(size, target, instr) {
+    constructor(size, instr) {
         this.size = size;
-        this.target = target;
         
-        this.is = instr.map((s) => {
+        const instructions = instr.map((s) => {
             // so everything can be expressed as a form of ((t * a) - b)
             //  convert and then squish
             // two combined is:
@@ -15,80 +14,56 @@ class Deck {
             //  (t * aX) - (bX + y)
             if (s.indexOf('deal into new stack') === 0) {
                 // a-t === (t * -1) - (-a)
-                return {
-                    type: this.MulMinus,
-                    args: [-1, 1 - this.size],
-                };
+                return [-1, 1 - this.size];
             } else if (s.indexOf('cut') === 0) {
                 // (t - a) === (t * 1) - a
-                return {
-                    type: this.MulMinus,
-                    args: [1, Number(s.split(' ')[1])],
-                };
+                return [1, Number(s.split(' ')[1])];
             } else if (s.indexOf('deal with increment' === 0)) {
                 // (t * a) === (t * a) - 0
-                return {
-                    type: this.MulMinus,
-                    args: [Number(s.split(' ')[3]), 0],
-                };
+                return [Number(s.split(' ')[3]), 0];
             } else {
                 return null;
             }
         });
 
-        console.log(`Original function source: ${this.is.map((s) => {
-            return s.type.apply(this, s.args);
-        }).join('\n')}`);
+        const r = this.SimplifyArr(instructions);
+        this.a = r[0];
+        this.b = r[1];
+    }
 
-        // crush instructions into a single line
-        while (this.is.length > 1) {
-            const newSquishedMethod = this.Simplify(this.is[0], this.is[1]);
-            this.is.splice(0, 2, newSquishedMethod);
-        }
-
-        console.log(`\nOptimised down to: ${this.is.map((s) => {
-            return s.type.apply(this, s.args);
-        }).join('\n')}`);
-
-        this.funcSrc = this.is.map((s) => {
-            return s.type.apply(this, s.args);
-        }).join('\n');
-
-        // turn string into a callable function
-        this.func = new Function(this.funcSrc);
+    Run(x) {
+        return this.Mul(x, this.a, this.b);
     }
 
     ModToSize(a) {
         return ((a % this.size) + this.size) % this.size;
     }
 
-    Simplify(a, b) {
-        // take two instructions and combine them
-        return {
-            type: this.MulMinus,
-            args: [
-                this.ModToSize(a.args[0]) * b.args[0],
-                this.ModToSize((a.args[1] * b.args[0]) + b.args[1]),
-            ],
-        }
+    SimplifyArr(arr) {
+        return arr.reduce((p, n) => {
+            return this.Simplify(p, n);
+        }, [1, 0])
     }
 
-    MulMinus(num1, num2) {
-        return `this.target = ((((this.target * ${num1}) - ${num2}) % ${this.size}) + ${this.size}) % ${this.size};`;
+    Simplify(a, b) {
+        // take two instructions and combine them
+        return [
+            this.ModToSize(this.ModToSize(a[0]) * this.ModToSize(b[0])),
+            this.ModToSize((a[1] * b[0]) + this.ModToSize(b[1])),
+        ];
+    }
+
+    Mul(x, num1, num2) {
+        return this.ModToSize((x * num1) - num2);
     }
 }
 
 Advent.GetInput().then((input) => {
-    const D = new Deck(10007, 2019, input);
-    D.func(); // run once
-    return Advent.Submit((D.target + D.size) % D.size).then(() => {
-        /*const D = new Deck(119315717514047, 2020, input);
-        const perc = 101741582076661 / 100;
-        for(let i=0; i<101741582076661; i++) {
-            if ((i % perc) === 0) console.log('.');
-            D.func();
-        }
-        return Advent.Submit((D.target + D.size) % D.size, 2);*/
+    const D = new Deck(10007, input);
+    const answer = D.Run(2019);
+    return Advent.Submit(answer).then(() => {
+        //const D = new Deck(119315717514047, input);
+        //const answer2 = D.Run(2020, 101741582076661);
     });
 }).catch((e) => {
     console.log(e);
