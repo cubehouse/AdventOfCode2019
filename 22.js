079494e65880e14d617d22a770dd2158c79f4a5d
cubehouse <me@jamie.holdings>
2019-12-27 00:06:06 +0000

    [+] Simplify part 1 solution * Another stab at part 2

--- 22.js
+++ 22.js
@@ -5,62 +5,56 @@ class Deck {
         this.size = size;
         
         const instructions = instr.map((s) => {
-            // so everything can be expressed as a form of ((t * a) - b)
-            //  convert and then squish
-            // two combined is:
-            //  ((t * a) - b)   +++    ((t * X) - Y)
-            //  ((t * X) - Y) * a) - b
-            //    ===
-            //  (t * aX) - (bX + y)
+            // each instruction can be expressed as a form of ((t * a) + b)
+            //  given a1, b1 and a2, b2 from two sequential operations:
+            //  (((t * a1) + b1) * a2) + b2
+            //   ===
+            //  (t * a1 * a2) + ((b1 * a1) + b2)
+            //  so after smushing, a = a1 * a2, and b = (b1 * a1) + b2
+            //  (see CombineSteps function)
             if (s.indexOf('deal into new stack') === 0) {
-                // a-t === (t * -1) - (-a)
-                return [-1, 1 - this.size];
+                // s-t === (t * -1) + (s - 1)
+                return [-1, -1]; // no need to actually add deck size since it will be modulo'd away
             } else if (s.indexOf('cut') === 0) {
-                // (t - a) === (t * 1) - a
-                return [1, Number(s.split(' ')[1])];
+                // (t - a) === (t * 1) + -a
+                return [1, -Number(s.split(' ')[1])];
             } else if (s.indexOf('deal with increment' === 0)) {
-                // (t * a) === (t * a) - 0
+                // (t * a) === (t * a) + 0
                 return [Number(s.split(' ')[3]), 0];
             } else {
                 return null;
             }
         });
 
-        const r = this.SimplifyArr(instructions);
-        this.a = r[0];
-        this.b = r[1];
+        const squished = this.CombineSteps(instructions);
+        this.a = squished[0];
+        this.b = squished[1];
     }
 
-    Run(x) {
-        return this.Mul(x, this.a, this.b);
+    Step(input, a, b) {
+        if (a === undefined) {
+            return this.Mod((this.a * input) + this.b);
+        }
+        return this.Mod((a * input) + b);
     }
 
-    ModToSize(a) {
-        return ((a % this.size) + this.size) % this.size;
-    }
-
-    SimplifyArr(arr) {
-        return arr.reduce((p, n) => {
-            return this.Simplify(p, n);
+    CombineSteps(steps) {
+        return steps.reduce((p, n) => {
+            return [
+                this.Mod(p[0] * n[0]),
+                this.Mod((p[1] * n[0]) + n[1]),
+            ];
         }, [1, 0])
     }
 
-    Simplify(a, b) {
-        // take two instructions and combine them
-        return [
-            this.ModToSize(this.ModToSize(a[0]) * this.ModToSize(b[0])),
-            this.ModToSize((a[1] * b[0]) + this.ModToSize(b[1])),
-        ];
-    }
-
-    Mul(x, num1, num2) {
-        return this.ModToSize((x * num1) - num2);
+    Mod(input) {
+        return ((input + this.size) % this.size) % this.size;
     }
 }
 
 Advent.GetInput().then((input) => {
     const D = new Deck(10007, input);
-    const answer = D.Run(2019);
+    const answer = D.Step(2019);
     return Advent.Submit(answer).then(() => {
         //const D = new Deck(119315717514047, input);
         //const answer2 = D.Run(2020, 101741582076661);
